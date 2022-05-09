c7e2a2e0bed6ca3708e91ddaec8f1252fa534914
tonnon <lucastonnon@gmail.com>
2022-05-09 20:04:01 -0300

    base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/feedback-widget-web/',
+  base: '/feedback-widget/',
   plugins: [react()]
 })
