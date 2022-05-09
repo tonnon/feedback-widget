bcf248671a3ef4f09b4e9826c3cb8ce4cfda1ece
tonnon <lucastonnon@gmail.com>
2022-05-09 20:06:33 -0300

    dist

--- server/src/shared/container/providers/index.ts
+++
@@ -1,9 +0,0 @@
-import { container } from 'tsyringe';
-
-import { NodeMailerMailProvider } from './mail-provider/implementations/nodemailer-mail-provider';
-import { MailProvider } from './mail-provider/mail-provider';
-
-container.registerSingleton<MailProvider>(
-  'MailProvider',
-  NodeMailerMailProvider
-);
--- server/src/shared/infra/http/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import { feedbacksRouter } from './feedbacks-routes';
-
-const routes = Router();
-
-routes.use(feedbacksRouter);
-
-export { routes };
